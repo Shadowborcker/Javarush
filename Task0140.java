972ab4fe5a114c02c125470ef204c974c3aaf103
Shadowbrocker <Shadowbrocker@gmail.com>
2018-07-24 08:21:45 +0300

    Level 1 Task 040

---
+++ Task0140.java
@@ -0,0 +1,13 @@
+package com.company;
+
+/*
+Дана переменная number.
+Напиши программу, которая выводит на экран квадрат этой переменной (number * number).
+ */
+public class Task0140 {
+    public static int number = 25;
+
+    public static void main(String[] args) {
+        System.out.print(Math.pow(number, 2));    //напишите тут ваш код
+    }
+}
