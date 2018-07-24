a5e98e84d63d59da3ce6815edcfe0c30cb2f4a90
Shadowbrocker <Shadowbrocker@gmail.com>
2018-07-24 08:22:44 +0300

    Level 2 Task 01

---
+++ Task0201.java
@@ -0,0 +1,20 @@
+package com.company;
+
+/*
+Дана переменная number.
+Напиши программу, которая выводит на экран квадрат этой переменной (number * number).
+ */
+public class Task0201 {
+    public static void main(String[] args) {
+        print("Java easy to learn!");
+        print("Java opens many opportunities!");
+    }
+
+    public static void print(String s) {
+     int i = 0;
+        while (i<4) {
+            System.out.println(s);
+            i++; //напишите тут ваш код
+        }
+    }
+}
