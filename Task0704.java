8f3391e26bac5d246bcf70430c3051133e3d86f7
Shadowbrocker <Shadowbrocker@gmail.com>
2018-07-31 15:15:40 +0300

    Level 7 Task 04 Reversing an array od ints

---
+++ Task0704.java
@@ -0,0 +1,39 @@
+
+/*
+1. Создать массив на 10 чисел.
+2. Ввести с клавиатуры 10 чисел и записать их в массив.
+3. Вывести на экран элементы массива в обратном порядке, каждое значение выводить с новой строки.
+
+
+Требования:
+1. Программа должна создавать массив на 10 целых чисел.
+2. Программа должна считывать числа для массива с клавиатуры.
+3. Программа должна выводить 10 строчек, каждую с новой строки.
+4. Массив должен быть выведен на экран в обратном порядке.
+*/
+
+
+package com.company;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+
+public class Task0704 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int[] array = new int[10];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = Integer.parseInt(reader.readLine());
+        }
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[array.length - 1 - i]);
+        }
+
+
+    }
+
+}
