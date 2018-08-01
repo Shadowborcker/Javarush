c694628fa06f22ef442f0ee3b7dec7167896a37e
Shadowbrocker <Shadowbrocker@gmail.com>
2018-08-01 22:56:04 +0300

    Level 7 Task 22 - Adding strings in ArrayList untill keyword is entered

---
+++ Task0722.java
@@ -0,0 +1,52 @@
+
+/*
+Создать список строк.
+Ввести строки с клавиатуры и добавить их в список.
+Вводить с клавиатуры строки, пока пользователь не введет строку "end". Саму строку "end" не учитывать.
+Вывести строки на экран, каждую с новой строки.
+
+
+Требования:
+1. Объяви переменную типа список строк и сразу проинициализируй ee.
+2. Считывай строки с клавиатуры и добавляй их в список, пока пользователь не введет строку "end".
+3. Саму строку "end" не нужно добавлять в список.
+4. Выведи список на экран, каждое значение с новой строки.
+5. Используй цикл for.
+*/
+
+
+package com.company;
+
+
+import java.io.*;
+import java.util.*;
+
+
+public class Task0722 {
+
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 0; i < Integer.MAX_VALUE; i++) {
+            String s = reader.readLine();
+            list.add(s);
+            if (s.equals("end")) {
+                list.remove(i);
+                break;
+            }
+        }
+
+
+        for (String s : list)
+            System.out.println(s);
+    }
+
+
+}
+
+
+
+
+
+
