b73fbde7f9e825c530fcd29c74ab446794a422b3
Shadowbrocker <Shadowbrocker@gmail.com>
2018-08-02 16:05:48 +0300

    Level 7 Task 27 - multiplying elements inside ArrayList

---
+++ Task0727.java
@@ -0,0 +1,59 @@
+
+/*
+Требования:
+Задача: Программа вводит строки, пока пользователь не введёт пустую строку (нажав enter).
+Потом она конвертирует строки в верхний регистр (Мама превращается в МАМА) и выводит их на экран.
+
+Новая задача: Программа вводит строки, пока пользователь не введёт пустую строку (нажав enter).
+Потом программа строит новый список. Если в строке чётное число букв, строка удваивается, если нечётное - утраивается.
+Программа выводит содержимое нового списка на экран.
+Требования:
+1. Программа должна считывать данные с клавиатуры.
+2. Программа должна вывести столько же строк, сколько было введено.
+3. Если в строке было четное число букв, то необходимо удвоить строку (как в примере).
+4. Если в строке было нечетное число букв, то необходимо утроить строку (как в примере).
+5. Программа должна прекращать считывать данные с клавиатуры как только пользователь введет пустую строку и нажмет Enter.
+*/
+
+
+package com.company;
+
+
+import java.io.*;
+import java.util.*;
+
+
+public class Task0727 {
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        ArrayList<String> list = new ArrayList<String>();
+        while (true) {
+            String s = reader.readLine();
+            if (s.isEmpty()) break;
+            list.add(s);
+        }
+
+        ArrayList<String> newlist = new ArrayList<>();
+        for (int i = 0; i < list.size(); i++) {
+            String s = list.get(i);
+            if (s.length() % 2 == 0) {
+                s = s + " " + s;
+                newlist.add(s);
+            } else {
+                s = s + " " + s + " " + s;
+                newlist.add(s);
+
+            }
+
+
+        }
+
+        for (String x : newlist) {
+            System.out.println(x);
+
+        }
+    }
+}
