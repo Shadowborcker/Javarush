d79637769dd4aea2993ebfd14342c6d62db07f3f
Shadowbrocker <Shadowbrocker@gmail.com>
2018-08-02 17:52:33 +0300

    Level 7 Task 28 - sorting and reversing an array

---
+++ Task0728.java
@@ -0,0 +1,48 @@
+
+/*
+Задача: Написать программу, которая вводит с клавиатуры 20 чисел и выводит их в убывающем порядке.
+
+
+Требования:
+1. Программа должна считывать 20 целых чисел с клавиатуры.
+2. Программа должна выводить 20 чисел.
+3. В классе Solution должен быть метод public static void sort(int[] array).
+4. Метод main должен вызывать метод sort.
+5. Метод sort должен сортировать переданный массив по убыванию.
+*/
+
+
+package com.company;
+
+
+import java.io.*;
+import java.util.*;
+
+
+public class Task0728 {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int[] array = new int[20];
+        for (int i = 0; i < 20; i++) {
+            array[i] = Integer.parseInt(reader.readLine());
+        }
+
+        sort(array);
+
+        for (int x : array) {
+            System.out.println(x);
+        }
+    }
+
+    public static void sort(int[] array) {
+        Arrays.sort(array);
+        int[] newarray = new int[20];
+        for (int i = 0; i < 20; i++) {
+            newarray[i] = array[19-i];
+        }
+        for (int i = 0; i < 20; i++) {
+            array[i] = newarray[i];
+        }
+    }
+}
