1087d42682823f416d258a25d6d56fa18832564c
Shadowbrocker <shadowbrocker@gmail.com>
2018-08-08 19:00:08 +0300

    Level 8 Task 26 - sorting an Array with descending bubble

--- Task0826.java
+++ Task0826.java
@@ -43,11 +43,11 @@ public class Task0826 {
     public static void sort(int[] array) {
         int temp;
         for (int i = 0; i < array.length; i++) {
-            for (int j = 1; j < array.length - 1; j++) {
-                if (array[j-1] < array[j]) {
-                    temp = array[j-1];
-                    array[j-1] = array[j];
-                    array[j] = temp;
+            for (int j = 0; j < array.length - 1; j++) {
+                if (array[j] < array[j+1]) {
+                    temp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = temp;
                 }
             }
         }
