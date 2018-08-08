d38133285d94b16d6c10b9dced36ef888dc9cb08
Shadowbrocker <shadowbrocker@gmail.com>
2018-08-08 20:47:50 +0300

    Level 8 Task 28 - return month number from map

---
+++ Task0828.java
@@ -0,0 +1,42 @@
+
+/*
+Программа вводит с клавиатуры имя месяца и выводит его номер на экран в виде: "May is the 5 month".
+Используйте коллекции.
+
+
+Требования:
+1. Программа должна считывать одно значение с клавиатуры.
+2. Программа должна выводить текст на экран.
+3. Программа должна использовать коллекции.
+4. Программа должна считывать с клавиатуры имя месяца и выводить его номер на экран по заданному шаблону.
+*/
+
+
+package com.company;
+
+import java.util.*;
+import java.io.*;
+
+
+public class Task0828 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String s = reader.readLine();
+        HashMap<String, Integer> months = new HashMap<String, Integer>();
+        months.put("January", 1);
+        months.put("February", 2);
+        months.put("March", 3);
+        months.put("April", 4);
+        months.put("May", 5);
+        months.put("June", 6);
+        months.put("July", 7);
+        months.put("August", 8);
+        months.put("September", 9);
+        months.put("October", 10);
+        months.put("November", 11);
+        months.put("December", 12);
+        System.out.println(s + " is the " + months.get(s) + " month");
+
+    }
+}
