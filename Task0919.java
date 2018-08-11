7da90d7b4881ebfc8a30f79d6f93f15d6cb7362f
Shadowbrocker <shadowbrocker@gmail.com>
2018-08-11 11:07:05 +0300

    level 9 Task 19 - stacktrace for division by zero

---
+++ Task0919.java
@@ -0,0 +1,43 @@
+
+/*
+Создай метод public static void divisionByZero, в котором подели любое число на ноль и выведи на экран результат деления.
+Оберни вызов метода divisionByZero в try..catch. Выведи стек-трейс исключения используя метод exception.printStackTrace()
+
+
+Требования:
+1. В классе должен быть метод public static void divisionByZero().
+2. Метод divisionByZero должен содержать операцию деления любого числа на ноль.
+3. Метод divisionByZero должен вызывать System.out.println или System.out.print.
+4. Метод main должен содержать блок try..catch.
+5. Метод main должен отлавливать любые исключения метода divisionByZero.
+6. Программа должна выводить стек-трейс пойманого исключения.
+*/
+
+
+package com.company;
+
+import java.io.*;
+
+
+public class Task0919 {
+
+    public static void main(String[] args) {
+        try {
+            divisionByZero();
+        }
+        catch (Exception exception) {
+            exception.printStackTrace();
+            }
+        }
+
+    public static void divisionByZero() {
+        System.out.println(10/0);
+    }
+
+
+}
+
+
+
+
+
