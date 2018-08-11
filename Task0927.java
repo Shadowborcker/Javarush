efdf8b1515e177b2c56d76ae6615d7e149e88bc5
Shadowbrocker <shadowbrocker@gmail.com>
2018-08-11 15:32:37 +0300

    level 9 Task 27 - creating a set from map values and printing it

---
+++ Task0927.java
@@ -0,0 +1,70 @@
+
+/*
+Есть класс кот - Cat, с полем "имя" (String).
+Создать словарь Map<String, Cat> и добавить туда 10 котов в виде "Имя"-"Кот".
+Получить из Map множество(Set) всех котов и вывести его на экран.
+
+
+Требования:
+1. Программа не должна считывать данные с клавиатуры.
+2. Метод createMap должен создавать новый объект HashMap.
+3. Метод createMap должен добавлять в словарь 10 котов в виде «Имя»-«Кот».
+4. Метод createMap должен возвращать созданный словарь.
+5. Метод convertMapToSet должен создать и вернуть множество котов, полученных из переданного словаря.
+6. Программа должна вывести всех котов из множества на экран.
+*/
+
+
+package com.company;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+
+
+public class Task0927 {
+
+    public static void main(String[] args) {
+        Map<String, Cat> map = createMap();
+        Set<Cat> set = convertMapToSet(map);
+        printCatSet(set);
+    }
+
+    public static Map<String, Cat> createMap() {
+        String[] catnames = new String[]{"васька", "мурка", "дымка", "рыжик", "серый", "снежок", "босс", "борис", "визя", "гарфи"};
+        HashMap<String, Cat> cats = new HashMap<>();
+        for (int i = 0; i < catnames.length; i++) {
+            Cat cat = new Cat(catnames[i]);
+            cats.put(catnames[i], cat);
+        }
+        return cats;
+    }
+
+    public static Set<Cat> convertMapToSet(Map<String, Cat> map) {
+        HashSet<Cat> set = new HashSet<>();
+        for (Map.Entry<String, Cat> pair : map.entrySet()) {
+            set.add(pair.getValue());
+        }
+        return set;
+    }
+
+    public static void printCatSet(Set<Cat> set) {
+        for (Cat cat : set) {
+            System.out.println(cat);
+        }
+    }
+
+    public static class Cat {
+        private String name;
+
+        public Cat(String name) {
+            this.name = name;
+        }
+
+        public String toString() {
+            return "Cat " + this.name;
+        }
+    }
+
+
+}
