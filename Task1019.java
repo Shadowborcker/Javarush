c3a91e8fcc2dbc14474a103292f9a71435c94b33
Shadowbrocker <shadowbrocker@gmail.com>
2018-08-16 18:50:51 +0300

    Level 10 Task 19 - Filling a hashmap and printing it

---
+++ Task1019.java
@@ -0,0 +1,63 @@
+
+/*
+Задача: Программа вводит с клавиатуры пару (число и строку) и выводит их на экран.
+Новая задача: Программа вводит с клавиатуры пары (число и строку), сохраняет их в HashMap.
+Пустая строка - конец ввода данных.
+Числа могут повторяться.
+Строки всегда уникальны.
+Введенные данные не должны потеряться!
+Затем программа выводит содержание HashMap на экран.
+Каждую пару с новой строки.
+
+Пример ввода:
+1
+Мама
+2
+Рама
+1
+Мыла
+
+Пример вывода:
+1 Мыла
+2 Рама
+1 Мама
+
+
+Требования:
+1. Программа должна считывать данные с клавиатуры.
+2. Программа должна выводить текст на экран.
+3. В методе main объяви переменную коллекции HashMap и сразу проинициализируй ee.
+4. Программа должна помещать в HashMap пары считанные с клавиатуры.
+5. Программа должна выводить на экран содержимое HashMap согласно условию. Ключ и значение разделены пробелом. Каждое значение с новой строки.
+*/
+
+
+package com.company;
+
+import java.io.*;
+import java.nio.file.FileSystemException;
+import java.util.*;
+import java.text.*;
+
+
+public class Task1019 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        HashMap<String, Integer> map = new HashMap<>();
+        while (true) {
+            try {
+                int id = Integer.parseInt(reader.readLine());
+                String name = reader.readLine();
+                map.put(name, id);
+            } catch (Exception e) {
+                break;
+            }
+        }
+
+        for (Map.Entry<String, Integer> pair : map.entrySet()) {
+            String name = pair.getKey();
+            Integer index = pair.getValue();
+            System.out.println(index + " " + name);
+        }
+    }
+}
