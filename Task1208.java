b41a98a658d32db97b700d4568b7c2467a88a042
Nikolay <shadowbrocker@gmail.com>
2018-08-21 09:00:22 +0300

    level 12 Task 08 - More overloads.

---
+++ Task1208.java
@@ -0,0 +1,35 @@
+package com.company;
+
+/*
+Написать пять методов print с разными параметрами.
+
+
+Требования:
+1. Программа не должна выводить текст на экран.
+2. Класс Solution должен содержать шесть методов.
+3. Класс Solution должен содержать статический метод main().
+4. Класс Solution должен содержать пять статических методов print() с разными параметрами.
+*/
+
+public class Task1208 {
+    public static void main(String[] args) {
+
+    }
+
+    public static void print(int a) {
+
+    }
+
+    public static void print(Integer a) {
+
+    }
+    public static void print(String a) {
+
+    }
+    public static void print(char a) {
+
+    }
+    public static void print(Boolean a) {
+
+    }
+}
