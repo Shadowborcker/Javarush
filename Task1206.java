5bacf3ecf29bed62e5bcfaaee8874e6b60872e5e
Nikolay <shadowbrocker@gmail.com>
2018-08-21 08:50:00 +0300

    Level 12 Task 06 - Method overload.

---
+++ Task1206.java
@@ -0,0 +1,27 @@
+package com.company;
+
+/*
+Написать два метода: print(int) и print(String).
+
+
+Требования:
+1. Программа не должна выводить текст на экран.
+2. Класс Solution должен содержать три метода.
+3. Класс Solution должен содержать статический метод main().
+4. Класс Solution должен содержать статический метод print() с параметром типа int.
+5. Класс Solution должен содержать статический метод print() с параметром типа String.
+*/
+
+public class Task1206 {
+    public static void main(String[] args) {
+
+    }
+
+    public static void pirnt(int a) {
+        System.out.println(a);
+    }
+
+    public static void print(String s) {
+        System.out.println(s);
+    }
+}
